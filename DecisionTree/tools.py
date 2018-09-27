a8085de4ace3b272fa56ad91311600e1786fb6ce
wangziyang01 <ziyangwang1992@163.com>
2018-09-27 21:16:36 +0800

    modify train with label

--- DecisionTree/tools.py
+++ DecisionTree/tools.py
@@ -20,29 +20,29 @@ from math import log
 
 
 def createDataset():
-    X = [[u"青绿", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑"], 
-    [u"乌黑", u"蜷缩", u"沉闷", u"清晰", u"凹陷", u"硬滑"],
-    [u"乌黑", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑"],
-    [u"青绿", u"蜷缩", u"沉闷", u"清晰", u"凹陷", u"硬滑"],
-    [u"浅白", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑"],
-    [u"青绿", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"软粘"],
-    [u"乌黑", u"稍蜷", u"浊响", u"稍糊", u"稍凹", u"软粘"],
-    [u"乌黑", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"硬滑"],
-    [u"乌黑", u"稍蜷", u"沉闷", u"稍糊", u"稍凹", u"硬滑"],
-    [u"青绿", u"硬挺", u"清脆", u"清晰", u"平坦", u"软粘"],
-    [u"浅白", u"硬挺", u"清脆", u"模糊", u"平坦", u"硬滑"],
-    [u"浅白", u"蜷缩", u"浊响", u"模糊", u"平坦", u"软粘"],
-    [u"青绿", u"稍蜷", u"浊响", u"稍糊", u"凹陷", u"硬滑"],
-    [u"浅白", u"稍蜷", u"沉闷", u"稍糊", u"凹陷", u"硬滑"],
-    [u"乌黑", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"软粘"],
-    [u"浅白", u"蜷缩", u"浊响", u"模糊", u"平坦", u"硬滑"],
-    [u"青绿", u"蜷缩", u"沉闷", u"稍糊", u"稍凹", u"硬滑"]]
+    dataset = [[u"青绿", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑", "yes"], 
+    [u"乌黑", u"蜷缩", u"沉闷", u"清晰", u"凹陷", u"硬滑", "yes"],
+    [u"乌黑", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑", "yes"],
+    [u"青绿", u"蜷缩", u"沉闷", u"清晰", u"凹陷", u"硬滑", "yes"],
+    [u"浅白", u"蜷缩", u"浊响", u"清晰", u"凹陷", u"硬滑", "yes"],
+    [u"青绿", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"软粘", "yes"],
+    [u"乌黑", u"稍蜷", u"浊响", u"稍糊", u"稍凹", u"软粘", "yes"],
+    [u"乌黑", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"硬滑", "yes"],
+    [u"乌黑", u"稍蜷", u"沉闷", u"稍糊", u"稍凹", u"硬滑", "no"],
+    [u"青绿", u"硬挺", u"清脆", u"清晰", u"平坦", u"软粘", "no"],
+    [u"浅白", u"硬挺", u"清脆", u"模糊", u"平坦", u"硬滑", "no"],
+    [u"浅白", u"蜷缩", u"浊响", u"模糊", u"平坦", u"软粘", "no"],
+    [u"青绿", u"稍蜷", u"浊响", u"稍糊", u"凹陷", u"硬滑", "no"],
+    [u"浅白", u"稍蜷", u"沉闷", u"稍糊", u"凹陷", u"硬滑", "no"],
+    [u"乌黑", u"稍蜷", u"浊响", u"清晰", u"稍凹", u"软粘", "no"],
+    [u"浅白", u"蜷缩", u"浊响", u"模糊", u"平坦", u"硬滑", "no"],
+    [u"青绿", u"蜷缩", u"沉闷", u"稍糊", u"稍凹", u"硬滑", "no"]]
     
-    y = ["yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "no", "no", "no", "no", "no", "no", "no", "no", "no"]
+    #y = ["yes", "yes", "yes", "yes", "yes", "yes", "yes", "yes", "no", "no", "no", "no", "no", "no", "no", "no", "no"]
     
     labels = [u'色泽', u'根蒂', u'敲声', u'纹理', u'脐部', u'触感']
 
-    return X, y, labels
+    return dataset, labels
 
     
 def get_max_label(y):
@@ -71,30 +71,30 @@ def get_entropy(y):
     return entropy 
 
 
-def split_dataset(X, y, index, value):
-    sub_X = []
+def split_dataset(dataset, y, index, value):
+    sub_dataset = []
     sub_y = []
 
-    for i in range(len(X)):
-        sample = X[i]
+    for i in range(len(dataset)):
+        sample = dataset[i]
         if sample[index] == value:
             new_sample = sample[:index]
             new_sample.extend(sample[index+1:])
-            sub_X.append(new_sample)
+            sub_dataset.append(new_sample)
             sub_y.append(y[i])
-    return sub_X, sub_y
+    return sub_dataset, sub_y
 
 
-def get_best_feat_index(X, y):
+def get_best_feat_index(dataset, y):
     base_entropy = get_entropy(y)
     max_info_gain = 0.0
     max_info_gain_index = -1
-    for index in range(len(X[0])):
+    for index in range(len(dataset[0]) - 1):
         info_gain = base_entropy
-        feat_values = set([sample[index] for sample in X])
+        feat_values = set([sample[index] for sample in dataset])
         for value in feat_values:
-            sub_X, sub_y = split_dataset(X, y, index, value) 
-            weight = float(len(sub_X)) / len(X)           
+            sub_dataset, sub_y = split_dataset(dataset, y, index, value) 
+            weight = float(len(sub_dataset)) / len(dataset)           
             info_gain -= weight * get_entropy(sub_y)
 
         print("index: %d, info_gain: %f" % (index, info_gain))
@@ -105,20 +105,34 @@ def get_best_feat_index(X, y):
     return max_info_gain_index 
 
 
-def create_tree(X, y, labels):
-    sample_num = len(X)
-    feat_num = len(X[0])
+def create_tree(dataset, labels):
+    sample_num = len(dataset)
+    y = [sample[-1] for sample in dataset]
+    feat_num = len(dataset[0]) - 1
     if feat_num == 0:
         return get_max_label(y)
     if y.count(y[0]) == len(y):
         return y[0]
-    best_feat_index = get_best_feat_index(X, y)
+    best_feat_index = get_best_feat_index(dataset, y)
     best_feat_label = labels[best_feat_index]
     tree = {best_feat_label:{}}
     del(labels[best_feat_index])
-    best_feat_values = set([sample[best_feat_index] for sample in X])
+    best_feat_values = set([sample[best_feat_index] for sample in dataset])
     for value in best_feat_values:
         sub_labels = labels[:]
-        sub_X, sub_y = split_dataset(X, y, best_feat_index, value)
-        tree[best_feat_label][value] = create_tree(sub_X, sub_y, sub_labels) 
+        sub_dataset, sub_y = split_dataset(dataset, y, best_feat_index, value)
+        tree[best_feat_label][value] = create_tree(sub_dataset, sub_labels) 
     return tree
+
+
+def predict_one(x_dict, tree):
+    x_dict = dict(zip(x, label))
+    pass
+
+
+def predict(X, label, tree):
+    for sample in X:
+        if len(x) != len(label):
+            sys.stderr.write("[ERROR] sample: %s and label: %s have different columns.\n" % (x, label))
+            return False 
+        predict_one(sample, label, tree)         
--- DecisionTree/tree.py
+++ DecisionTree/tree.py
@@ -18,6 +18,9 @@ Brief         : 决策树ID3
 import sys
 
 from tools import create_tree
+from tools import predict
+from tools import save_model
+from tools import load_model
 
 
 class DecisionTree(object):
@@ -32,17 +35,33 @@ class DecisionTree(object):
         self.tree = None
         self.labels = None
 
-
-    def fit(self, X, y, labels):
-        if len(X) != len(y):
-            sys.stderr.write("[FATAL] Your sample and label have different nums.\n")
-            return False
-        if len(X) == 0 or len(y) == 0:
+    def fit(self, dataset, labels):
+        if len(dataset) == 0 or len(labels) == 0:
             sys.stderr.write("[FATAL] your sample or label is null.\n")
+            return False 
+        if len(dataset[0]) != len(labels) - 1:
+            sys.stderr.write("[FATAL] your sample and label have different columns.\n")
             return False
+        if len(dataset[0]) < 3:
+            sys.stderr.write("[FATAL] please make sure your feature nums are more than 1.\n")
         self.labels = labels
-        self.tree = create_tree(X, y, labels[:])
+        self.tree = create_tree(dataset, labels[:])
         return True
 
-    def predict(self, X):
+    def predict(self, X, labels):
+        if self.tree is None:
+            sys.stderr.write("[FATAL] your tree has not been created or loaded.\n")
+            return False
+        if X is None:
+            sys.stderr.write("[FATAL] your input dataset is none, please recheck.\n")
+            return False
+        if len(X[0] != len(labels)):
+            sys.stderr.write("[FATAL] your sample and label have different columns.")
+            return False
+        result = predict(X, labels, self.tree)
+
+    def save_model(self, path):
+        pass
+
+    def load_mode(self, path):
         pass
